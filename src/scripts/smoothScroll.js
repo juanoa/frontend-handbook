2ea02409e8ebb8965d3ca873afc9a6e4c3e9774e
Juan Otálora <juanoa.otalora@gmail.com>
2025-01-26 22:01:38 +0100

    chore: add sitemap and upgrade dependencies

---
+++ src/scripts/smoothScroll.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute("href");
+      const targetElement = document.querySelector(targetId);
+
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        });
+
+        // Update URL without triggering a scroll
+        history.pushState(null, "", targetId);
+      }
+    });
+  });
+});
